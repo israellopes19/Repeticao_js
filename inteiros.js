3fa7f9d5e00a096c9438c59352774ce4638453db
Israellopes <143666711+israellopes19@users.noreply.github.com>
2024-05-20 19:36:10 -0300

    Add files via upload

--- inteiros.js
+++ inteiros.js
@@ -1,20 +1,18 @@
 var entrada = require ("readline-sync");
 
-var numero;
+var numero = 0;
 var par = 0;
 var impar = 0;
 
    
 for(var i=0; i<10; i++){
     var numero=parseInt(entrada.question('Digite um numero: '));
-}
 
-for( var i =0; i<=numero; i++ ){
     if(numero%2 === 0){
         par++;
     }else{
         impar++;
     }
 }
-   console.log('Quantudade de números pares:' + par);
-   console.log('Quantidade de números impares:' +impar);
+   console.log('Quantudade de números pares:', + par ,'e a quantidade de impares:' +impar);
+  
